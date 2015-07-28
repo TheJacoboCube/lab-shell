f27463522342b144c3509c7f477b41b4020401e3
Carson Killbreath <kill4992@kettering.edu>
2015-07-28 18:49:01 -0400

    Build function to take input. Also forks new process for execution. Still untested.

--- shell.c
+++ shell.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #define MAX_LINE 80
+#define BUFFER_START 20
 
-char * get_input()
-{
-    return "cat shell.c";
-}
+char * get_input();
+void fail_with_error(const char * message);
 
 int main(void)
 {
@@ -17,25 +18,64 @@ int main(void)
         printf("stephen> ");
         fflush(stdout);
 
-        /**
-        * After reading user input, the steps are:
-        * (1) fork a child process using fork()
-        * (2) the child process will execvp()
-        * (3) if command included &, parent will invoke wait()
-        */
-
         input = get_input(); // Get input from user.
+
+        if(strlen(input) == 0)
+        {
+            free(input);
+            continue;
+        }
+
         args = strtok(input, " "); // Tokenize the args.
+        free(input);
 
         // TODO: Check for ampersand.
-
-        pid_t pid = fork();
-
-        if(pid == 0)
+        
+        if(fork() == 0)
         {
             execvp(args[0], args); // Execute command.
         }
     }
 
     return 0;
+}
+
+void fail_with_error(const char * message)
+{
+    printf("Error: %s", message);
+    exit(0);
+}
+
+char * get_input()
+{
+    unsigned int bufferSize = BUFFER_START, bufferUsed = 0;
+    char * buffer = calloc(bufferSize, sizeof(char));
+
+    while (1) {
+        char currentChar = getchar();
+
+        if(currentChar == '\n')
+        {
+            break;
+        }
+
+        buffer[bufferUsed] = currentChar;
+
+        if (bufferUsed == bufferSize - 1) { /* buffer full */
+            bufferSize *= 2; // Double the size.
+            // Bigger buffer, please! :D
+            buffer = (char *) realloc(buffer, bufferSize);
+
+            if(buffer == NULL) // Aw, por que no?
+            {
+                fail_with_error("Could not get memory space for buffer.");
+            }
+        }
+
+        bufferUsed++;
+    }
+
+    buffer[bufferUsed] = '\0'; // Zero string.
+
+    return buffer;
 }
